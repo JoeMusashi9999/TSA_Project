94ae4c9f9d4cb7061081efcdbd846f692748766e
Abhay Raj <71041500+GithubDev939@users.noreply.github.com>
2023-01-04 15:30:02 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -119,6 +119,16 @@ camera.lookAt(0, 0, 0);
 
 let timestep = 0.0;
 
+window.addEventListener('resize', function()
+
+    {
+      var width = window.innerWidth;
+      var height = window.innerHeight;
+      renderer.setSize(width, height);
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+});
+
 var render = function () {
     requestAnimationFrame(render);
 
