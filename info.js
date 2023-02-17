b4c72ce0f74afb10758f186acc76679f5e6f9c5c
Joe Musashi <derek.borden.999@gmail.com>
2023-02-16 20:08:24 -0800

    implement float down function--not done!--

--- info.js
+++ info.js
@@ -1,11 +1,14 @@
+let completedUp=false; 
 window.addEventListener("scroll", function() {
   const companyNameSection = document.querySelector(".company-name-section");
   const distanceFromTop = companyNameSection.getBoundingClientRect().top;
   const windowHeight = window.innerHeight;
 
-  if (distanceFromTop <= 0 && distanceFromTop >= -windowHeight) {
+  if (distanceFromTop <= 0 && distanceFromTop >= -windowHeight && !completedUp) {
     companyNameSection.classList.add("float-up");
-  } else {
-    companyNameSection.classList.remove("float-up");
+    completedUp=true;
+  } else if (distanceFromTop == 0 && completedUp){
+    companyNameSection.classList.add("float-down");
+    completedUp=false;
   }
 });
