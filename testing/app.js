a3adb1b204ba9d64eea090f17dc7cf7e7849b9b7
davin00 <davin.aoyama@misd400.org>
2023-03-09 22:18:09 -0800

    update

--- testing/app.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector(".burger");
-const navLinks = document.querySelector(".nav-links");
-
-burger.addEventListener("click", () => {
-  navLinks.classList.toggle("nav-active");
-});
