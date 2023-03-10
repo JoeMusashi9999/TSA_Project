8d48b6f605aeda39d583066b9f2cd9d111bd8592
davin00 <davin.aoyama@misd400.org>
2023-03-09 22:29:53 -0800

    doing

--- testing/script.js
+++ testing/script.js
@@ -1,17 +1,5 @@
-const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
-const navLinksLi = document.querySelectorAll('.nav-links li');
-
-burger.addEventListener('click', () => {
-  navLinks.classList.toggle('nav-active');
-
-  navLinksLi.forEach((link, index) => {
-    if (link.style.animation) {
-      link.style.animation = '';
-    } else {
-      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-    }
+$(document).ready(function() {
+  $('.burger').click(function() {
+    $('.nav-links').toggleClass('nav-active');
   });
-
-  burger.classList.toggle('toggle');
 });
