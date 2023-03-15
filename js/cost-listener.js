900ff29d617eba5c6ff9a2acbbca07d49163694a
Derek <derek.borden.999@gmail.com>
2023-03-14 17:32:22 -0800

    beta cost projection

--- js/cost-listener.js
+++ js/cost-listener.js
@@ -1,10 +1,18 @@
 const select = document.getElementById("type");
+const destination = document.getElementById("destination");
 const output = document.getElementById("output");
+var type = "";
+
+
 
 select.addEventListener("change", (event) => {
   const selectedOption = event.target.value;
   if (selectedOption === "Commercial") {
-    output.textContent = "Projected cost:";
+    
+    if (destination === "mars"){
+    output.textContent = "Projected cost: 57000 TerraUnits";
+    }
+    
   } else if (selectedOption === "Personal") {
     output.textContent = "Projected cost:";
   } else if (selectedOption === "Governement") {
@@ -12,4 +20,17 @@ select.addEventListener("change", (event) => {
   } else if (selectedOption === "none") {
     output.textContent = "Please select an option.";
   }
+});
+
+
+destination.addEventListener("change", (event) =>{
+  const destination = event.target.value;
+  if (destination === "mars"){
+    output.textContent = "Projected cost: 57000 TerraUnits";
+    }
+});
+
+
+$.get('https://theskylive.com/mercury-tracker', null, function(text){
+  alert($(text).find('#disearth'));
 });
