85ffe5f37bedcdfc5d3f24eca9dfe9c97c9c6459
Derek <derek.borden.999@gmail.com>
2023-03-14 19:49:02 -0800

    stuff

--- js/cost-listener.js
+++ js/cost-listener.js
@@ -8,26 +8,67 @@ var type = "";
 select.addEventListener("change", (event) => {
   const selectedOption = event.target.value;
   if (selectedOption === "Commercial") {
-    
-    if (destination === "mars"){
-    output.textContent = "Projected cost: 57000 TerraUnits";
-    }
-    
+    type = "Commercial";
   } else if (selectedOption === "Personal") {
-    output.textContent = "Projected cost:";
+    type = "Personal";
   } else if (selectedOption === "Governement") {
-    output.textContent = "Projected cost:";
+    output.textContent = "For government contracts, contact us directly";
   } else if (selectedOption === "none") {
-    output.textContent = "Please select an option.";
+    type = "none";
   }
 });
 
 
 destination.addEventListener("change", (event) =>{
   const destination = event.target.value;
-  if (destination === "mars"){
-    output.textContent = "Projected cost: 57000 TerraUnits";
-    }
+  if (destination === "mercury" && type ==="Personal"){
+    output.textContent = "Projected cost:  TerraUnits";
+    } else if (destination === "murcury" && type ==="Commercial"){
+      output.textContent = "Projected cost:  TerraUnits";
+      }
+
+        if (destination === "venus" && type ==="Personal"){
+          output.textContent = "Projected cost: 57000 TerraUnits";
+        }else if (destination === "venus" && type ==="Commercial"){
+        output.textContent = "Projected cost: 57000 TerraUnits";
+        }
+        if (destination === "mars" && type ==="Personal"){
+          output.textContent = "Projected cost: 57000 TerraUnits";
+          }else if (destination === "mars" && type ==="Commercial"){
+          output.textContent = "Projected cost: 57000 TerraUnits";
+          }
+
+        if (destination === "jupiter" && type ==="Personal"){
+          output.textContent = "Projected cost:  TerraUnits";
+          } else if (destination === "jupiter" && type ==="Commercial"){
+            output.textContent = "Projected cost:  TerraUnits";
+            }
+
+            if (destination === "saturn" && type ==="Personal"){
+              output.textContent = "Projected cost:  TerraUnits";
+              } else if (destination === "saturn" && type ==="Commercial"){
+                output.textContent = "Projected cost:  TerraUnits";
+                }
+
+                if (destination === "uranus" && type ==="Personal"){
+                  output.textContent = "Projected cost:  TerraUnits";
+                  } else if (destination === "uranus" && type ==="Commercial"){
+                    output.textContent = "Projected cost:  TerraUnits";
+                    }
+                    if (destination === "neptune" && type ==="Personal"){
+                      output.textContent = "Projected cost:  TerraUnits";
+                      } else if (destination === "neptune" && type ==="Commercial"){
+                        output.textContent = "Projected cost:  TerraUnits";
+                        }
+                        if (destination === "pluto" && type ==="Personal"){
+                          output.textContent = "Projected cost:  TerraUnits";
+                          } else if (destination === "pluto" && type ==="Commercial"){
+                            output.textContent = "Projected cost:  TerraUnits";
+                            }
+
+
+     
+     
 });
 
 
